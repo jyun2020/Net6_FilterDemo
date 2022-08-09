cd0f90f076a83e7f95fd41fe9e7c5a4a43c4f070
delia850331 <delia850331@gmail.com>
2022-08-09 22:53:32 +0800

    ?

---
+++ Net6_FilterDemo/Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Net6_FilterDemo.Filters;
+
+namespace Net6_FilterDemo.Controllers
+{
+    public class DemoController : Controller
+    {
+        public DemoController()
+        {
+            Console.WriteLine("DemoController建構子");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [CustomResourceFilter]
+        public IActionResult Resource()
+        {
+            Console.WriteLine("進入Action");
+            return View();
+        }
+    }
+}
---
+++ Net6_FilterDemo/Filters/CustomResourceFilterAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Net6_FilterDemo.Filters
+{
+    public class CustomResourceFilterAttribute : Attribute, IResourceFilter
+    {
+        //模擬快取
+        private Dictionary<string,object> Cache = new Dictionary<string,object>();
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            Console.WriteLine("離開ResourceFilter");
+        }
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            Console.WriteLine("進入ResourceFilter");
+        }
+    }
+}
